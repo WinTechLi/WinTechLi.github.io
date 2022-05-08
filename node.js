b2ceaee164228156f940c555e626520a5cdd51b1
WinTechLi <105121419+WinTechLi@users.noreply.github.com>
2022-05-08 16:06:58 +0100

    Create node.js

---
+++ node.js
@@ -0,0 +1,26 @@
+const client = require('@sendgrid/client');
+client.setApiKey(process.env.SENDGRID_API_KEY);
+
+const id = 4900;
+const headers = {
+  "on-behalf-of": "The subuser's username. This header generates the API call as if the subuser account was making the call."
+};
+const data = {
+  "ip": "192.168.0.1"
+};
+
+const request = {
+  url: `/v3/whitelabel/domains/${id}/ips`,
+  method: 'POST',
+  headers: headers,
+  body: data
+}
+
+client.request(request)
+  .then(([response, body]) => {
+    console.log(response.statusCode);
+    console.log(response.body);
+  })
+  .catch(error => {
+    console.error(error);
+  });
